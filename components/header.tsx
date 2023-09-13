2de5690b6d1ef5c3e8fe17c7b75d3b61ba3bcd97
Spike <florian@playtoday.cc>
2023-09-13 23:49:39 +1000

    Header with context

--- components/header.tsx
+++ components/header.tsx
@@ -5,10 +5,11 @@ import { motion } from 'framer-motion';
 import { links } from '@/lib/data';
 import Link from 'next/link';
 import clsx from 'clsx';
+import { useActiveSectionContext } from '@/context/active-section-context';
 
 const Header = () => {
-  const [activeSection, setActiveSection] = useState('Home');
-  const [timeOfLastClick, setTimeOfLastClick] = useState(Date.now());
+  const { activeSection, setActiveSection, setTimeOfLastClick } =
+    useActiveSectionContext();
 
   return (
     <header className="z-[999] relative">
